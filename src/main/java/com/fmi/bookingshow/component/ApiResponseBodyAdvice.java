6ad3423c0edfc827366c0cb0eb4ecbdd4ae07536
anicoi <alexandru.nicoi@s.unibuc.ro>
2024-05-12 17:11:22 +0300

    Fixes on CI

--- src/main/java/com/fmi/bookingshow/component/ApiResponseBodyAdvice.java
+++ src/main/java/com/fmi/bookingshow/component/ApiResponseBodyAdvice.java
@@ -1,5 +1,6 @@
 package com.fmi.bookingshow.component;
 
+import com.fmi.bookingshow.constants.Apis;
 import com.fmi.bookingshow.dto.response.ResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
@@ -9,6 +10,8 @@ import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 @Slf4j
@@ -22,6 +25,13 @@ public class ApiResponseBodyAdvice implements ResponseBodyAdvice<Object> {
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
                                       Class<? extends HttpMessageConverter<?>> selectedConverterType,
                                       ServerHttpRequest request, ServerHttpResponse response) {
+        List<String> responseDtoPaths = List.of(Apis.AUTH, Apis.CORE, Apis.ADMIN);
+        log.info("Writing to %s".formatted(request.getURI().getPath()));
+        for(String path : responseDtoPaths) {;
+            if (!request.getURI().getPath().startsWith(path)) {
+                return body;
+            }
+        }
         if (body instanceof ResponseDto<?>) {
             return body;
         }
